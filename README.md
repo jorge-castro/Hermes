e77105b9bff35035d463eb4dbba08e319023a6de
Jorge Castro <jorgec535@protonmail.com>
2020-10-21 22:56:47 -0700

    Update project

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Hermes
 
+:warning: THIS IS A WORK IN PROGRESS :warning:
+
 Hermes is a Telegram bot that provides data on different financial assets.
---
+++ bot_commands.py
@@ -0,0 +1,16 @@
+import requests
+import yfinance as yf
+
+#Queries the coingecko API and returns basic info on the coin(s) as deserialized json data
+def get_crypto_price(ids, vs_currencies="usd", include_market_cap="false", include_24hr_vol="false", include_24hr_change="false", include_last_updated_at="false"):
+    return requests.get(f"https://api.coingecko.com/api/v3/simple/price?ids={ids}&vs_currencies={vs_currencies}&include_market_cap={include_market_cap}&include_24hr_vol={include_24hr_vol}&include_24hr_change={include_24hr_change}&include_last_updated_at={include_last_updated_at}").json()
+
+#Takes a str of space-separated tickers and returns stock price in the format {ticker:market_price}
+def get_stock_price(tickers):
+    stocks = yf.Tickers(tickers).tickers
+    return {stock.ticker: stock.info["regularMarketPrice"] for stock in stocks}
+
+#Takes a dict in the format {ticker:no_of_shares} and returns a dict containing {ticker:{no_of_shares:x, worth_of_shares:y}}
+def get_worth_of_shares(shares):
+    prices = get_stock_price(" ".join(shares.keys()))
+    return {ticker.upper(): {"no_of_shares": shares[ticker], "worth_of_shares": shares[ticker]*prices[ticker.upper()]} for ticker in shares.keys()}
