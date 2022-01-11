1a4d41775e870481339d8aa43065795f1526e38d
Jorge Castro <jorgec535@protonmail.com>
2022-01-11 01:11:30 -0800

    Fix yfinance related functions and add start and help commands

--- bot_commands.py
+++ bot_commands.py
@@ -1,10 +1,20 @@
 import requests
 import yfinance as yf
 
+
+#Greet the user when they interact with the bot for the first time
+def start():
+    return "Hello! I'm Hermes, your new financial assistant."
+
+
+#Show a help message to the user
+def help():
+    return "I'm a bot that can provide the price of different financial assets"
+
+
 #Queries the coingecko API and returns basic info on the coin(s) as
 #deserialized json data
-
-def get_crypto_price(ids, vs_currencies="usd", include_market_cap="false",
+def get_crypto_price(ids="handshake", vs_currencies="usd", include_market_cap="false",
                      include_24hr_vol="false", include_24hr_change="false",
                      include_last_updated_at="false"):
     
@@ -19,16 +29,14 @@ def get_crypto_price(ids, vs_currencies="usd", include_market_cap="false",
 
 #Takes a str of space-separated tickers and returns stock price in the
 #format {ticker:market_price}
-
-def get_stock_price(tickers):
+def get_stock_price(tickers="MSFT GOOG"):
     stocks = yf.Tickers(tickers).tickers
-    return {stock.ticker: stock.info["regularMarketPrice"] for stock in stocks}
+    return {ticker: stock.info["regularMarketPrice"] for ticker, stock in stocks.items()}
 
 
 #Takes a dict in the format {ticker:no_of_shares} and returns a dict
 #containing {ticker:{no_of_shares:x, worth_of_shares:y}}
-
-def get_worth_of_shares(shares):
+def get_worth_of_shares(shares={"MSFT":10}):
     prices = get_stock_price(" ".join(shares.keys()))
     return {ticker.upper(): {"no_of_shares": shares[ticker],
                              "worth_of_shares": shares[ticker]*prices[ticker.upper()]}
