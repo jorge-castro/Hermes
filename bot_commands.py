52d4608d299966315fe91f430d5aeeb6780dc4d9
Jorge Castro <jorgec535@protonmail.com>
2022-01-13 00:34:49 -0800

    Streamline functions and ensure they all return string values

--- bot_commands.py
+++ bot_commands.py
@@ -1,3 +1,4 @@
+from json import dumps
 import requests
 import yfinance as yf
 
@@ -9,37 +10,36 @@ def start():
 
 #Show a help message to the user
 def help():
-    return "I'm a bot that can provide the price of different financial assets"
+    return "I'm a bot that can provide information on different financial " \
+           "assets, i.e. the price of various cryptocurrencies and stocks.\n" \
+           "Select a command to see usage examples."
 
 
-#Queries the coingecko API and returns basic info on the coin(s) as
-#deserialized json data
-def get_crypto_price(ids="handshake", vs_currencies="usd", include_market_cap="false",
+#Queries the coingecko API and returns basic info on the coin(s)
+def get_crypto_price(ids, vs_currencies="usd", include_market_cap="false",
                      include_24hr_vol="false", include_24hr_change="false",
                      include_last_updated_at="false"):
     
-    return requests.get(f"https://api.coingecko.com/api/v3/simple/price"
-                        f"?ids={ids}"
-                        f"&vs_currencies={vs_currencies}"
-                        f"&include_market_cap={include_market_cap}"
-                        f"&include_24hr_vol={include_24hr_vol}"
-                        f"&include_24hr_change={include_24hr_change}"
-                        f"&include_last_updated_at={include_last_updated_at}").json()
+    r = requests.get(f"https://api.coingecko.com/api/v3/simple/price"
+                     f"?ids={ids}"
+                     f"&vs_currencies={vs_currencies}"
+                     f"&include_market_cap={include_market_cap}"
+                     f"&include_24hr_vol={include_24hr_vol}"
+                     f"&include_24hr_change={include_24hr_change}"
+                     f"&include_last_updated_at={include_last_updated_at}")
+
+    return dumps(r.json(), indent=4)
 
 
-#Takes a str of space-separated tickers and returns stock price in the
-#format {ticker:market_price}
-def get_stock_price(tickers="MSFT GOOG"):
+#Takes a string of space-separated tickers and returns stock price(s) in the
+#format "{ticker:market_price}"
+def get_stock_price(tickers):
     stocks = yf.Tickers(tickers).tickers
-    return {ticker: stock.info["regularMarketPrice"] for ticker, stock in stocks.items()}
 
+    prices = {ticker: stock.info["regularMarketPrice"]
+              for ticker, stock in stocks.items()
+              if stock.info["regularMarketPrice"] != None}
 
-#Takes a dict in the format {ticker:no_of_shares} and returns a dict
-#containing {ticker:{no_of_shares:x, worth_of_shares:y}}
-def get_worth_of_shares(shares={"MSFT":10}):
-    prices = get_stock_price(" ".join(shares.keys()))
-    return {ticker.upper(): {"no_of_shares": shares[ticker],
-                             "worth_of_shares": shares[ticker]*prices[ticker.upper()]}
-            for ticker in shares.keys()}
+    return dumps(prices, indent=4)
 
 
