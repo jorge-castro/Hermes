86dc0637da5b1350f62a81b39ca3ea327aba9347
Jorge Castro <jorgec535@protonmail.com>
2022-01-13 00:35:34 -0800

    Implement bot commands

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 from inspect import getmembers, isfunction
-from json import dumps
+
 import requests
 from flask import Flask, request
 from dotenv import load_dotenv
+
 import bot_commands as bot
+
 
 #Load API token from .env
 load_dotenv()
@@ -20,11 +22,6 @@ COMMANDS = {member[0]:member[1] for member in getmembers(bot, isfunction)}
 app = Flask(__name__)
 
 
-#Wrapper for json.dumps() with paramaters set to return a pretty string
-def beautiful_dumps(data):
-    return dumps(data, sort_keys=True, indent=4)
-
-
 #Meet Hermes!
 @app.route("/")
 def index():
@@ -37,23 +34,18 @@ def server():
     #Get update data
     data = request.get_json()
 
-    print(beautiful_dumps(data)) #For debugging
-
     #Only handle new messages, ignore other kinds of updates
-    if "message" not in data.keys():
+    if "message" not in data:
         return "Thank you for the update!"
 
     #Define useful variables
     message = data["message"]
     chat = message["chat"]
     chat_id = chat["id"]
-    user = message["from"]
-    user_id = user["id"]
 
     #Check for optional fields in the message data
-    fields = message.keys()
-    entities = message["entities"] if "entities" in fields else None
-    text = message["text"] if "text" in fields else None
+    entities = message["entities"] if "entities" in message else None
+    text = message["text"] if "text" in message else None
 
     #Define variable to hold bot command if one is found
     command = None
@@ -67,22 +59,41 @@ def server():
                 command = text[offset+1:length] #Add 1 to offset due to "/"
                 break
 
-    #If no valid command is found send a message to the user indicating so,
-    #otherwise send appropriate response
+    #Define variable to hold reply
+    reply = "" 
+
+    #If no valid command is found send a message to the user indicating so
     if command not in COMMANDS:
-        return {
-                "method": "sendMessage",
-                "chat_id": chat_id,
-                "text": "Please send a valid command"
-                }
+        reply = "Please send a valid command. Use /help for more information."
+
+    #Handle /get_crypto_price
+    elif command == "get_crypto_price":
+        reply = COMMANDS[command](",".join(text[offset+length:].split()))
+
+        if reply == "{}":
+            reply = "Please pass valid identifier(s) to the command.\n\n" \
+                    "E.g., /get_crypto_price bitcoin handshake\n\n" \
+                    "See https://api.coingecko.com/api/v3/coins/list " \
+                    "for a full list."
+
+    #Handle /get_stock_price
+    elif command == "get_stock_price":
+        reply = COMMANDS[command](" ".join(text[offset+length:].split()))
+
+        if reply == "{}":
+            reply = "Please pass valid ticker(s) to the command.\n\n" \
+                    "E.g., /get_stock_price MSFT GOOG\n\n" \
+                    "See https://stockanalysis.com/stocks/ for a full list."
+
+    #Handle /start and /help
     else:
-        #TODO: find a way to pass arguments; right now this only works if
-        #function corresponding to command works without passing arguments
-        #and if return value can be processed by json.dumps()
-        return {
-                "method": "sendMessage",
-                "chat_id": chat_id,
-                "text": beautiful_dumps(COMMANDS[command]())
-                }
+        reply = COMMANDS[command]()
+    
+    #Send message with appropriate reply to the chat 
+    return {
+        "method": "sendMessage",
+        "chat_id": chat_id,
+        "text": reply
+        }
 
 
